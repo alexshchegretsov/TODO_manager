26bae044710c442b9b7f3d10ecc4af60cbdb7b19
alex_shchegretsov <nydollz77@gmail.com>
2019-06-08 01:38:28 +0300

    Correct Edit view

--- src/todo_app/views/edit_task_controller.py
+++ src/todo_app/views/edit_task_controller.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from todo_app.models import Task
 from todo_app.forms import TaskForm
 
 
 def edit_task(request, task_id):
-    task = Task.objects.get(id=task_id)
+    task = get_object_or_404(Task, id=task_id)
     if request.method == 'GET':
         context = {'form': TaskForm(initial={'content': task.content})}
         return render(request, 'todo_app/edit.html', context)
