ae815da418cd677e1bedf0426704cd7d0f624dff
alex_shchegretsov <nydollz77@gmail.com>
2019-06-09 01:27:20 +0300

    Add background image and folder images for readme

--- src/todo_app/urls.py
+++ src/todo_app/urls.py
@@ -6,9 +6,6 @@ from .views import delete_task
 from .views import to_up_task
 from .views import to_down_task
 from .views import mark_done
-from .views import set_priority_high
-from .views import set_priority_medium
-from .views import set_priority_low
 from .views import filter_by_priority
 
 urlpatterns = [
@@ -19,8 +16,5 @@ urlpatterns = [
     path('<int:task_id>/', to_up_task, name='up_task_url'),
     path('down/<int:task_id>/', to_down_task, name='down_task_url'),
     path('done/<int:task_id>/', mark_done, name='mark_done_url'),
-    path('priority-high/<int:task_id>/', set_priority_high, name='set_priority_high_url'),
-    path('priority-medium/<int:task_id>/', set_priority_medium, name='set_priority_medium_url'),
-    path('priority-low/<int:task_id>/', set_priority_low, name='set_priority_low_url'),
     path('filter/', filter_by_priority, name='filter_by_priority_url'),
 ]
