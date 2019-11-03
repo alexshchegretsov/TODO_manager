39f68db642b057836c04a0bbe6204b53cb1bf683
alex_shchegretsov <nydollz77@gmail.com>
2019-11-03 12:30:37 +0300

    Add README

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3
+MAINTAINER Alex Shchegretsov <a.shchegretsov@gmail.com>
+WORKDIR /app
+COPY ./src/requirements.txt /app
+RUN pip install -r requirements.txt
+COPY ./src /app
--- src/todo_engine/settings.py
+++ src/todo_engine/settings.py
@@ -76,12 +76,10 @@ WSGI_APPLICATION = 'todo_engine.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
@@ -123,8 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIRS = 'static'
+# create "staticfiles" dir after ./manage.py collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# additional static-dirs
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
